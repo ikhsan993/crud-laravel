465fdccaabfe4f170463c296f706507e57b26e27
ikhsan993 <amirulikhsan993@gmail.com>
2021-07-12 10:35:03 +0700

    adding read data from db

--- app/Models/GuruModel.php
+++ app/Models/GuruModel.php
@@ -3,28 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class GuruModel extends Model
 {
     public function allData()
     {
-       return [
-            [
-                'nip' => '1234',
-                'nama' => 'rudi',
-                'mapel' => 'Biologi'
-            ],
-            [
-                'nip' => '1233',
-                'nama' => 'Sudir',
-                'mapel' => 'Fisika'
-            ],
-            [
-                'nip' => '1237',
-                'nama' => 'Lina',
-                'mapel' => 'Kimia'
-            ]
-            ];
+        return DB::table('guru')->get();
     }
 
 
--- resources/views/guru.blade.php
+++ resources/views/guru.blade.php
@@ -2,11 +2,34 @@
 @section('title','Guru')
 
 @section('content')
-@foreach ($guru as $data)
-NIP : {{ $data['nip'] }} <br>
-NAMA : {{ $data['nama'] }} <br>
-MAPEL: {{ $data['mapel'] }} <br>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>NIP</th>
+            <th>Nama Guru</th>
+            <th>Mata Pelajaran</th>
+            <th>Foto Guru</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $no=1; ?>
+        @foreach ($guru as $data )
+            <tr>
+                <td>{{ $no++ }}</td>
+                <td>{{ $data ->nip}}</td>
+                <td>{{ $data ->nama_guru}}</td>
+                <td>{{ $data ->mapel}}</td>
+                <td><img src="{{ url('fotoguru/'.$data->foto_guru) }}" width="100px"></td>
+                <td>
+                    <a href="" class="btn btn-sm btn-success">Detail</a>
+                    <a href="" class="btn btn-sm btn-warning">Edit</a>
+                    <a href="" class="btn btn-sm btn-danger">Delete</a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 <br>
-@endforeach
 
 @endsection
